3979423085b499219a607ff1bb499d8ea9357344
theman2457019 <164942268+theman2457019@users.noreply.github.com>
2025-01-11 11:12:39 -0700

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 1;        //4B
+        uint xx = 8;      //$B positive
+        long y = 9l;      //8B
+        float gpa = 3.563F;  // 4B -> 7-8 SD
+        double gpa2 = 3.34567898; // 8B -> 17 SD
+        string name = "mason";
+        bool happy = true;
+
         Console.WriteLine("Hello Sandbox World!");
+
+        System.Console.WriteLine(gpa);
+        System.Console.WriteLine("Hello, my name is Mason.");
+
+
+        string firstName = "";
+        System.Console.WriteLine("Enter first Name");
+        System.Console.ReadLine(firstName);
+
+
+        if (a < 10 && b > 10)  // and
+        {
+
+        }
+        if (a < 10 || b > 10)  // or
+        {
+
+        }
+        if (a ! 10)  // not
+        {
+
+        }
+
     }
 }
