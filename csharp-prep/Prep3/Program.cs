ddc72a18e948866da7cb41fbd2fa578d21c8ecbe
theman2457019 <164942268+theman2457019@users.noreply.github.com>
2025-01-14 12:48:15 -0700

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Concurrent;
 
 Random randomGenerator = new Random();
-int number = randomGenerator.Next(1, 11);
+int number = randomGenerator.Next(1, 101);
 //System.Console.WriteLine(number);
 
 bool Correct = false;
+System.Console.WriteLine("Guess a number between 1 and 100");
 while (!Correct)
 
 {
@@ -25,14 +26,14 @@ while (!Correct)
         System.Console.WriteLine("Congratulations! You guessed the number!");
         Correct = true;
 
-        System.Console.Write("Play Again?y/n");
+        System.Console.Write("Play Again?y/n:");
         string Again = Console.ReadLine().ToLower();
 
         if (Again == "y")
         {
             Correct = false;
-            number = randomGenerator.Next(1, 11); // Generate a new random number
-                Console.WriteLine("A new number between 1 and 10 has been chosen!");
+            number = randomGenerator.Next(1, 101); // Generate a new random number
+                Console.WriteLine("A new number between 1 and 100 has been chosen!");
             
             
         }
