ed5dea72b3ad27eb26e5861de57cba0f76fb76dc
theman2457019 <164942268+theman2457019@users.noreply.github.com>
2025-01-15 19:01:31 -0700

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,28 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        System.Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        System.Console.Write("Please Enter your name:");
+        string Name = Console.ReadLine();
+        return Name;
+    }
+    static double PromptUserNumber()
+    {
+        Console.Write("Please enter your Favorite number:");
+        double number = Double.Parse(Console.ReadLine());
+        return number;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        double number = PromptUserNumber();
+        double numberSquared = number * number;
+        System.Console.WriteLine($"{userName}, the sqare of your number is {numberSquared}");
     }
 }
