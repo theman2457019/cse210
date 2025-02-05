655adca978d6537fd43306ef4f705bda34935c4e
theman2457019 <164942268+theman2457019@users.noreply.github.com>
2025-02-05 12:19:40 -0700

    Learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction(3, 4);
+        System.Console.WriteLine(f1.GetFractionString());
+        System.Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        System.Console.WriteLine(f2.GetFractionString());
+        System.Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(1,3);   
+        System.Console.WriteLine(f3.GetFractionString());
+        System.Console.WriteLine(f3.GetDecimalValue());
     }
 }
